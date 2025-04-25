841b4aa783c7ec70facdb7e6915787a1d21e7e01
helloworld0118 <49739894+helloworld0118-dev@users.noreply.github.com>
2025-04-25 10:03:02 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-//
-//  Package.swift
-//  SwiftyChrono
-//
-//  Created by Potix on 2017-01-17.
-//  Copyright © 2017 Potix.
-//
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyChrono",
-    dependencies: []
+    products: [
+        .library(name: "SwiftyChrono", targets: ["SwiftyChrono"])
+    ],
+    targets: [
+        .target(name: "SwiftyChrono", dependencies: []),
+        .testTarget(name: "SwiftyChronoTests", dependencies: ["SwiftyChrono"])
+    ]
 )
